95e8bdc2ac85129b6670f9bcdfb3586c521b9e02
Vitaly05 <vitalik050405@gmail.com>
2024-06-24 10:43:48 +0300

    Init

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script>
+import { RouterView } from 'vue-router'
+
+export default {
+  components: {
+    RouterView
+  }
+}
+</script>
+
+<template>
+  <RouterView />
+</template>
+
+<style scoped>
+
+</style>

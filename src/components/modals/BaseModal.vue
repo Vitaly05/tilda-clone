c2ba22008d1d1c77c2be6c1b6f64ca80d5147dab
Vitaly05 <vitalik050405@gmail.com>
2024-06-26 21:27:11 +0300

    Fixed modals overlay when open multiple modals

--- src/components/modals/BaseModal.vue
+++ src/components/modals/BaseModal.vue
@@ -1,10 +1,6 @@
 <template>
   <div v-for="(modal, index) in openedModals" class="modal modal--open">
-    <div
-      class="modal__backdrop"
-      :class="{ 'modal__backdrop--first': index === 0 }"
-      @click="close"
-    ></div>
+    <div v-show="index === openedModals.length - 1" class="modal__backdrop" @click="close"></div>
     <div class="modal__content-wrap">
       <component :is="modal"></component>
     </div>
